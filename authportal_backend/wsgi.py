1b955c9935172ea17cc1deb2691c09613a91f06d
Shivani Mishra <mishrashivani8780@gmail.com>
2025-08-12 09:08:04 +0530

    chnage  commit

--- authportal_backend/wsgi.py
+++ authportal_backend/wsgi.py
@@ -8,9 +8,8 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 """
 
 import os
-from django.core.asgi import get_asgi_application
+from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'authportal_backend.settings')
 
-application = get_asgi_application()
-
+application = get_wsgi_application()
